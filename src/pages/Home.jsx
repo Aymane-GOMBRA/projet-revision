35486bc9b5dd426e3a49d53e133eb173f8c4065b
Aymane-GOMBRA <agombra95@gmail.com>
2022-10-24 13:45:21 +0200

    0.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [squares, setSquares] = useState(50)
   return (
     <div>
-        <h1>Home</h1>
+      <h1>Etats</h1>
+      <h2>{squares}</h2>
+
+      <button onClick={() => { setSquares(150) }}>Modifier</button>
     </div>
   )
 }
