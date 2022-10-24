9a0f028084a6bea2293ccb0e272120e0fc8556cf
Aymane-GOMBRA <agombra95@gmail.com>
2022-10-24 12:22:35 +0200

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <Link to='/'>Home</Link>
+        </div>
+    );
+}
+
+export default Navbar;
