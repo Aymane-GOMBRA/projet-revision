14d775e09a4a095149b029179eb737f351b39ff8
Aymane-GOMBRA <agombra95@gmail.com>
2022-10-27 13:37:39 +0200

    stable

--- src/components/Slider/Thumbnails.jsx
+++ src/components/Slider/Thumbnails.jsx
@@ -5,7 +5,7 @@ const Thumbnails = ({ pictures, setSelectPic, selectPic }) => {
         <div className='containerThumbnails'>
             {
                 pictures.map((pic, i) => (
-                    <img draggable="false" key={i} onClick={(e) => { setSelectPic(i) }} style={i === selectPic ? { width: '120px', margin: '5px', border: 'solid 5px black' } : { width: '120px', margin: '5px' }} src={pic.link} />
+                    <img key={i} onClick={(e) => { setSelectPic(i) }} style={i === selectPic ? { width: '120px', margin: '5px', border: 'solid 5px black' } : { width: '120px', margin: '5px' }} src={pic.link} />
                 ))
             }
         </div>
